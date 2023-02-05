0c6d96f76083d322d920ade42f409b476d755b4a
Stefan Momcilovic <stefan.momcilovic@protonmail.com>
2023-02-05 01:58:54 +0100

    verified

--- generate.py
+++ generate.py
@@ -1,42 +1,44 @@
 import requests
-import random
-import hashlib
-from english_words import get_english_words_set
+import mnemonic
+import secrets
+from eth_account import Account
+
+
 INFURA_API_KEY = ""
 
 def generate_seed_phrase():
-    word_list = get_english_words_set(['web2'], lower=True)
-    seed_phrase = []
-    for i in range(12):
-        seed_phrase.append(random.choice(list(word_list)))
-    return " ".join(seed_phrase)
-
+    entropy = secrets.token_bytes(16)
+    m = mnemonic.Mnemonic("english")
+    return m.to_mnemonic(entropy)
 
 def get_balance(address):
     url = f"https://mainnet.infura.io/v3/{INFURA_API_KEY}/eth/balance/{address}?tag=latest"
     response = requests.get(url)
 
     if response.status_code == 200:
-        balance = float(response.json(), 16) / 10**18
-        return balance
+        balance = int(response.json(), 16) / 10**18
+        return float(balance)
     else:
-        return 0
+        return 0.00
 
 def generate_and_check_balance():
     seed_phrase = generate_seed_phrase()
-    address = f"0x{hashlib.sha3_256(seed_phrase.encode()).hexdigest()[24:]}"
+    acct = Account.create(seed_phrase)
+    address = acct.address
     balance = get_balance(address)
-    return address, balance, seed_phrase
+    return address, balance, acct._private_key, seed_phrase
 
 while True:
-    address, balance, seed_phrase = generate_and_check_balance()
+    address, balance, private_key, seed_phrase = generate_and_check_balance()
     if balance > 0.00:
         print('Found Balance!!!')
         print("Address:", address)
-        print("Balance:", balance, "ETH")
+        print("Balance: {:.3f} ETH".format(balance))
+        print("Private Key:", private_key.hex())
         print("Seed phrase:", seed_phrase)
         break
     else:
         print("Address:", address)
-        print("Balance:", balance, "ETH")
+        print("Balance: {:.3f} ETH".format(balance))
+        print("Private Key:", private_key.hex())
         print("Seed phrase:", seed_phrase)
